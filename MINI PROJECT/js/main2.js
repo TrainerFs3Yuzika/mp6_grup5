5f00c2b22650192646f0e2e1e132f3ae262c4a4a
icanyu <ihsanuddin8090@gmail.com>
2024-04-01 12:41:32 +0700

    up

---
+++ MINI PROJECT/js/main2.js
@@ -0,0 +1,295 @@
+var produk = [{
+        "id": 1,
+        "title": "Beras Sania 5 kg",
+        "description": "ABC Kopi Susu merupakan minuman kopi instan yang terbuat dari gula, krimer nabati, kopi bubuk, susu bubuk skim yang diproses secara moder",
+        "price": 14000,
+        "rating": 4.9,
+        "terjual": 94,
+        "brand": "ABC",
+        "thumbnail": "https://i.ibb.co/hY7W6cz/beras.png",
+        "images": [
+            "https://i.ibb.co/hY7W6cz/beras.png",
+            "https://i.ibb.co/cN5nSNX/beras2-removebg-preview.png",
+        ]
+    },
+    {
+        "id": 2,
+        "title": "MINYAK SUNCO 1 LITER",
+        "description": "SunCo dibuat dengan teknologi mutakhir dengan melalui 5 tahapan proses yakni 2 kali proses penyaringan dan 3 kali proses pemurnian sehingga menghasilkan minyak goreng yang baik",
+        "price": 16500,
+        "rating": 4.7,
+        "terjual": 89,
+        "brand": "Sunco",
+        "thumbnail": "https://i.ibb.co/zXh4YF6/minyak.png",
+        "images": [
+            "https://cdn.dummyjson.com/product-images/2/1.jpg",
+            "https://cdn.dummyjson.com/product-images/2/2.jpg",
+            "https://cdn.dummyjson.com/product-images/2/3.jpg",
+            "https://cdn.dummyjson.com/product-images/2/thumbnail.jpg"
+        ]
+    },
+    {
+        "id": 3,
+        "title": "GULAKU 1KG",
+        "description": "GULAKU diproduksi dari tebu segar bermutu baik dari perkebunan kami di Lampung dengan menggunakan proses produksi yang memenuhi standar mutu untuk menghasilkan gula yang manis, alami, murini, dan bersih.",
+        "price": 18000,
+        "rating": 4.9,
+        "terjual": 80,
+        "brand": "Gulaku",
+        "thumbnail": "https://i.ibb.co/RCjYn6J/gulaku.png",
+        "images": [
+            "https://cdn.dummyjson.com/product-images/3/1.jpg",
+            "https://cdn.dummyjson.com/product-images/2/1.jpg",
+            "https://cdn.dummyjson.com/product-images/2/1.jpg"
+        ]
+    },
+    {
+        "id": 4,
+        "title": "KOPI ABC SUSU 10 SACHET",
+        "description": "Kopi Susu yang benar benar kopi Campuran sempurna antara kopi bubuk dengan aroma yang khas, gula dan rasa susu yang nikmat, menjadikan kopi ABC susu, kopi susu yang benar benar kopi. Teman Mantap untuk bangkitkan semangat dan kreativitas kamu.",
+        "price": 14000,
+        "rating": 4.8,
+        "terjual": 88,
+        "brand": "ABC",
+        "thumbnail": "https://i.ibb.co/55zFVtD/abcsusu.png",
+        "images": [
+            "https://cdn.dummyjson.com/product-images/4/3.jpg",
+            "https://cdn.dummyjson.com/product-images/4/4.jpg",
+            "https://cdn.dummyjson.com/product-images/4/thumbnail.jpg"
+        ]
+    },
+    {
+        "id": 5,
+        "title": "INDOMIE GORENG 1 DUS",
+        "description": "Indomie Goreng Mie Instan [1 dus/40 pcs] merupakan mie goreng instant yang memiliki cita rasa yang gurih dan lezat. Terbuat menggunakan bahan tepung terigu, rempah-rempah dan bumbu terbaik, dengan tekstur yang lembut sehingga dapat dikonsumsi setiap saat kapanpun dan dimanapun, serta aman dikonsumsi. Mudah dibuat dengan penyajian yang tidak memakan waktu lama",
+        "price": 118000,
+        "rating": 4.3,
+        "terjual": 75,
+        "brand": "Indomie",
+        "thumbnail": "https://i.ibb.co/pKmXYF4/indomie.png",
+        "images": [
+
+            "https://cdn.dummyjson.com/product-images/4/2.jpg",
+            "https://cdn.dummyjson.com/product-images/4/3.jpg",
+            "https://cdn.dummyjson.com/product-images/4/4.jpg",
+            "https://cdn.dummyjson.com/product-images/4/thumbnail.jpg"
+        ]
+    },
+    {
+        "id": 6,
+        "title": "SABUN LIFEBUOY",
+        "description": "Lifebuoy Sabun Batang Anti Bakteri Total 10 dengan Multivitamin, 100% lebih kuat lawan kuman berbahaya. Beli sekarang!dan bumbu terbaik, dengan tekstur yang lembut sehingga dapat dikonsumsi setiap saat kapanpun dan dimanapun, serta aman dikonsumsi. Mudah dibuat dengan penyajian yang tidak memakan waktu lama",
+        "price": 35500,
+        "rating": 4.3,
+        "terjual": 93,
+        "brand": "Indomie",
+        "thumbnail": "https://i.ibb.co/R9VsvKt/sabunlifebuoy.png",
+        "images": [
+            "https://cdn.dummyjson.com/product-images/4/2.jpg",
+            "https://cdn.dummyjson.com/product-images/4/3.jpg",
+            "https://cdn.dummyjson.com/product-images/4/4.jpg",
+            "https://cdn.dummyjson.com/product-images/4/thumbnail.jpg"
+        ]
+    },
+
+    {
+        "id": 7,
+        "title": "AQUA 330ML 1 DUS",
+        "description": "Aqua botol 330 ml atau yang sering disebut aqua botol mini karena ukurannya yang kecil sehingga mudah dibawa saat berpergian atau buat bawa minum anak sekolah",
+        "price": 43000,
+        "rating": 4.6,
+        "terjual": 63,
+        "brand": "Indomie",
+        "thumbnail": "https://i.ibb.co/S5z8jJ8/aqua.png",
+        "images": [
+            "https://cdn.dummyjson.com/product-images/4/2.jpg",
+            "https://cdn.dummyjson.com/product-images/4/3.jpg",
+            "https://cdn.dummyjson.com/product-images/4/4.jpg",
+            "https://cdn.dummyjson.com/product-images/4/thumbnail.jpg"
+        ]
+    },
+    {
+        "id": 8,
+        "title": "KOPI KAPAL API 10 SACHET",
+        "description": "Kopi Kapal Api terbuat dari biji kopi pilihan dan diproses dengan mesin yang paling modern yang menghasilkan kopi berkualitas tinggi dengan Aroma terbaik dan Rasa yang enak. Kapal Api Special Mix Dibuat dari Biji kopi pilihan yang diolah dengan mesin yang paling modern dan campuran gula murni. Menjadikannya paduan spesial dari kopi dan gula.",
+        "price": 43000,
+        "rating": 4.6,
+        "terjual": 63,
+        "brand": "Indomie",
+        "thumbnail": "https://i.ibb.co/1vwqWyJ/kapalapi.png",
+        "images": [
+            "https://cdn.dummyjson.com/product-images/4/2.jpg",
+            "https://cdn.dummyjson.com/product-images/4/3.jpg",
+            "https://cdn.dummyjson.com/product-images/4/4.jpg",
+            "https://cdn.dummyjson.com/product-images/4/thumbnail.jpg"
+        ]
+    },
+
+]
+
+$("#cart-icon").click(function() {
+    $(".cart").addClass("active");
+    labelCart();
+})
+
+$("#close-cart").click(function() {
+    $(".cart").removeClass("active");
+})
+
+$(".search-produk").click(function() {
+    let name = $(".searchInput").val();
+    cariProduk(produk, name);
+})
+
+$(".searchInput").keyup(function() {
+    let name = $(this).val();
+    cariProduk(produk, name);
+})
+$(document).ready(function() {
+    // Tambahkan event listener untuk tombol "Beli Sekarang"
+    $(".btn-buy").click(function() {
+        buyButtonClicked();
+    });
+});
+
+
+
+function getAllProduk(product) {
+    var html = "";
+    for (var i = 0; i < product.length; i++) {
+        html += `
+      <div class="product-box">
+          <img src="${product[i].thumbnail}" alt="" class="product-img">
+          <h2 class="product-title">${product[i].title}</h2>
+          <span class="price">Rp${getFormatRupiah(product[i].price)}</span>
+          <h2 class="fa-solid fa-star" class="product-info"> ${product[i].rating} | ${product[i].terjual} terjual</h2>
+          <a href="detailproduk.html?id=${product[i].id}" class="tombol">Detail Produk</a>
+          <i onclick="addToCart(${product[i].id})" class="add-cart bx bx-cart">Keranjang</i>
+      </div>
+      `
+    }
+    $(".produk-tampil").html(html);
+}
+
+function buyButtonClicked() {
+    swal({
+        title: "Pesanan Anda Telah Ditempatkan",
+        icon: "success",
+        buttons: true,
+    }).then((confirmed) => {
+        if (confirmed) {
+            $(".cart-content").empty();
+            localStorage.removeItem('cart');
+            $(".total-price").text('Rp0');
+            $(".cart").removeClass("active");
+        }
+    });
+}
+$(document).ready(function() {
+    $(".btn-buy").click(function() {
+        var cart = JSON.parse(localStorage.getItem('cart')) || [];
+        if (cart.length === 0) {
+            swal("Keranjang Kosong", "Anda belum menambahkan produk ke dalam keranjang belanja.", "warning");
+        } else {
+            buyButtonClicked();
+        }
+    });
+});
+
+function cariProduk() {
+    var input = document.getElementById("searchInput").value.toUpperCase();
+    var productBoxes = document.querySelectorAll(".shop-content.produk-tampil .product-box");
+
+    productBoxes.forEach(function(box) {
+        var title = box.querySelector(".product-title").textContent.toUpperCase();
+
+        if (title.indexOf(input) > -1) {
+            box.style.display = "";
+        } else {
+            box.style.display = "none";
+        }
+    });
+
+
+    document.querySelector(".shop-content.produk-tampil").style.gridTemplateColumns = "repeat(auto-fill, minmax(200px, 1fr))";
+}
+
+function labelCart() {
+    var cart = JSON.parse(localStorage.getItem('cart')) || [];
+    let htmlCartlabel = ""
+    let total = 0;
+    cart.forEach((item, index) => {
+        htmlCartlabel += `
+      <div class="cart-box">
+        <img src="${item.img}" alt="" class="cart-img">
+        <div class="detail-box">
+            <div class="cart-product-title">${item.name}</div>
+            <div class="cart-price">Rp${getFormatRupiah(item.price)}</div>
+            <input type="number" onchange="updateCartQty(${item.id}, this.value)" value="${item.quantity}" class="cart-quantity">
+        </div>
+        <!-- Remove cart -->
+        <i onclick="removeCartItem(${item.id})" class="bx bxs-trash-alt cart-remove"></i>
+      </div>
+      `
+        total = (item.price * item.quantity);
+    })
+    $(".total-price").html('Rp' + getFormatRupiah(total));
+    $(".cart-content").html(htmlCartlabel);
+}
+
+// function removeCartItem(productId) {
+//     let cart = JSON.parse(localStorage.getItem('cart')) || [];
+//     const update = cart.filter(item => item.id == productId);
+//     const updatedCart = cart.filter(item => item.id !== productId);
+//     localStorage.setItem('cart', JSON.stringify(updatedCart));
+//     labelCart()
+// }
+
+function removeCartItem(productId) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    const update = cart.filter(item => item.id == productId);
+    const updatedCart = cart.filter(item => item.id !== productId);
+    localStorage.setItem('cart', JSON.stringify(updatedCart));
+    labelCart();
+    swal("Item berhasil dihapus", "", "success");
+}
+
+
+function addToCart(productId) {
+    let getProductId = produk.find(product => product.id === productId);
+
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    const existingProductIndex = cart.findIndex(item => item.id === productId);
+
+    if (existingProductIndex !== -1) {
+        cart[existingProductIndex].quantity++;
+    } else {
+        cart.push({
+            id: productId,
+            quantity: 1,
+            name: getProductId.title,
+            price: getProductId.price,
+            img: getProductId.thumbnail
+        })
+    }
+    localStorage.setItem('cart', JSON.stringify(cart));
+    $(".cart").addClass("active");
+    labelCart()
+}
+
+function updateCartQty(productId, quantity) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    console.log(cart)
+    const productIndex = cart.findIndex(item => item.id === productId);
+    if (productIndex !== -1) {
+        cart[productIndex].quantity = quantity;
+    }
+    localStorage.setItem('cart', JSON.stringify(cart));
+    labelCart()
+}
+
+getAllProduk(produk)
+
+function getFormatRupiah(number) {
+    return number.toLocaleString("id-ID");
+}
--- MINI PROJECT/js/pembayaran.js
+++
@@ -1,3 +0,0 @@
-function getProductStorange() {
-    localStorage.getItem('productList') ? productList = JSON.parse(localStorage.getItem('productList')) : productList = [];
-}
